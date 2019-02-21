8e9281beb0c6b7ca86a3bbe7403f50008d95437c
RLonskiy <RLonskiy@luxoft.com>
2019-02-21 21:19:40 +0600

    chore: project structure

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+class Hello {
+  constructor() {}
+
+  save() {
+
+
+                                        }
+}
+
+export const sum = (a: number, b: number): number => {
+  return a;
+}
+
+
